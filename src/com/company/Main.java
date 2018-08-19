e607f4d53b723b98e13a29c9f6d2939b86535a41
Maciej.J <maciej.jaremowicz@gmail.com>
2018-08-19 15:14:17 +0200

    added random numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,29 @@
 package com.company;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Losujemy liczby!");
+        System.out.print("Podaj ile: ");
+
+        int ile;
+        ile = scanner.nextInt();
+
+        Random random = new Random();
+        for (int i = 0; i < ile; i++) {
+            int x = random.nextInt(99 + 1);
+
+            System.out.println(x);
+
+
+        }
+
 
     }
 }
